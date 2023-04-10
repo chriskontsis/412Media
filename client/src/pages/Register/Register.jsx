3159a522c045ea3a4770ba83b82ba3066ebde429
chriskontsis <cak0927@gmail.com>
2023-04-10 11:29:43 -0700

    fixing server.js queries with postman had to update my table

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -11,8 +11,8 @@ const Register = () => {
   // Handle input changes and update the form data state
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+    console.log("handling change");
   };
-
 
   // Handle form submission
   const handleSubmit = async (e) => {
@@ -20,7 +20,7 @@ const Register = () => {
     try {
       // Send form data to the server
       await axios.post("/register", formData);
-      
+
       // Show success message
       alert("Registration successful");
     } catch (err) {
@@ -28,15 +28,12 @@ const Register = () => {
     }
   };
 
-
   return (
     <div className="register">
       <div className="card">
         <div className="left">
           <h1>CSE412 Media</h1>
-          <p>
-            Welcome to CSE412 Media Please Register Your Account.
-          </p>
+          <p>Welcome to CSE412 Media Please Register Your Account.</p>
           <span>Already have an account?</span>
           <Link to="/login">
             <button>Login</button>
@@ -45,16 +42,56 @@ const Register = () => {
         <div className="right">
           <h1>Register</h1>
           <form onSubmit={handleSubmit}>
-          <input type="text" name="username" placeholder="Username" onChange={handleChange} />
-          <input type="password" name="password" placeholder="Password" onChange={handleChange} />
-          <input type="text" name="firstName" placeholder="First Name" onChange={handleChange} />
-          <input type="text" name="lastName" placeholder="Last Name" onChange={handleChange} />
-          <input type="text" name="email" placeholder="Email" onChange={handleChange} />
-          <input type="text" name="hometown" placeholder="Hometown" onChange={handleChange} />
-          <input type="text" name="gender" placeholder="Gender" onChange={handleChange} />
-          <input type="text" name="dateOfBirth" placeholder="Date of Birth" onChange={handleChange} />
-          {/* Change button type to submit */}
-          <button type="submit">Register</button>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="lastName"
+              placeholder="Last Name"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="hometown"
+              placeholder="Hometown"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="gender"
+              placeholder="Gender"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="dateOfBirth"
+              placeholder="Date of Birth"
+              onChange={handleChange}
+            />
+            {/* Change button type to submit */}
+            <button type="submit">Register</button>
           </form>
         </div>
       </div>
--- server/server.js
+++ server/server.js
@@ -8,34 +8,40 @@ app.use(cors());
 // Parse incoming JSON payloads
 app.use(express.json());
 const contribution = 0;
+let user_id = 1;
 // Route for user registration
-app.post("/register",async(req,res)=>{
-    try {
-         // Destructure user data from the request body
-        const { user_id, password, fName, lName, email, hometown, gender, dob } = req.body;
-         // Insert new user data into the users table
-        const newUser = await pool.query(
-            "INSERT INTO users (user_id, password, fName, lName, email, hometown, gender, dob, contribution) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
-            [user_id, password, fName, lName, email, hometown, gender, dob, contribution]
-          );
-    
-          res.json(newUser.rows[0]);
-        } catch (err) {
-          console.error(err.message);
-        }
+app.post("/register", async (req, res) => {
+  try {
+    // Destructure user data from the request body
+    user_id++;
+    const id = user_id;
+    const { username, password, fname, lname, email, hometown, gender, dob } =
+      req.body;
+    const contrib = 0;
+    // Insert new user data into the users table
+    const newUser = await pool.query(
+      "INSERT INTO users (user_id, username, pwd, fname, lname, email, hometown, gender, dob, contribution) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *",
+      [
+        user_id,
+        username,
+        password,
+        fname,
+        lname,
+        email,
+        hometown,
+        gender,
+        dob,
+        contribution,
+      ]
+    );
+
+    res.json(newUser.rows[0]);
+    console.log("post");
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
-app.get("/getMedia", (req, res) => {
-    res.send("these are the restaurants");
-})
-
-
-
-
-
-
-
-
 app.listen(3005, () => {
-    console.log("server is up and listening on port 3005")
+  console.log("server is up and listening on port 3005");
 });
