780d494cfbe5c5e80bbdc79b7f1c065a59ed0b76
chriskontsis <cak0927@gmail.com>
2023-04-10 08:56:25 -0700

    User Profile Page with albums

---
+++ client/src/components/albums/Albums.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./albums.scss";
+import Album from "../album/Album";
+
+const Albums = () => {
+  const albums = [
+    {
+      id: 1,
+      name: "First Album",
+      userId: 1,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: 2,
+      name: "Second Album",
+      userId: 1,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: 2,
+      name: "Second Album",
+      userId: 1,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: 2,
+      name: "Second Album",
+      userId: 1,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: 2,
+      name: "Second Album",
+      userId: 1,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+  ];
+
+  return (
+    <div className="albums">
+      {albums.map((album) => (
+        <Album album={album} key={album.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Albums;
--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import "./profile.scss"
+import React from "react";
+import "./profile.scss";
+import Albums from "../../components/albums/Albums";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  return <Albums />;
+};
 
-export default Profile
+export default Profile;
